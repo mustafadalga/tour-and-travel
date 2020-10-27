dd05722c531dbb9a3ed0d7dc9970c3dee4b98c55
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-10-28 00:54:19 +0300

    Navigation menu responsive completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
+/* Navigation Menu */
+var nav = document.querySelector('nav');
+var hamburger_menu = document.querySelector('.hamburger-menu-wrap')
+var close_icon=document.querySelector('.nav-close-icon');
+hamburger_menu.addEventListener('click', (event) => {
+    nav.classList.add('open')
+});
+close_icon.addEventListener('click', (event) => {
+    nav.classList.remove('open')
+});
+
 // Custom Dropdown
 
 /* Resource:https://andrejgajdos.com/custom-select-dropdown/ */
